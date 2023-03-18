df82ab0a97b4bcc1cfe7feb68db3cbc7fe4cef14
defajha06 <128251087+defajha06@users.noreply.github.com>
2023-03-19 01:05:31 +0700

    Delete com.tutorial directory

--- com.tutorial/src/inputanJOptionPane.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class inputanJOptionPane {
-    public static void main(String[] args){
-        String name = JOptionPane.showInputDialog("Masukkan Nama: ");
-        JOptionPane.showMessageDialog(null, "Nama Anda : " + name);
-    }
-}
